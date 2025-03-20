a6985b28c78f9580ffb3792b3cef4f7c05047e03
Tom Dörr <tomdoerr96@gmail.com>
2025-03-20 16:12:51 +0100

    style: Add blank lines between test functions in test_xml_schema.py

--- tests/test_xml_schema.py
+++ tests/test_xml_schema.py
@@ -3,10 +3,12 @@
 from src.utils.xml_schema import get_schema  # pylint: disable=no-name-in-module
 from src.utils.xml_tools import validate_xml  # pylint: disable=no-name-in-module
 
+
 def test_schema_is_valid_xml():
     """Test that the schema itself is valid XML."""
     schema = get_schema()
     assert validate_xml(schema), "Schema should be valid XML"
+
 
 def test_schema_contains_required_elements():
     """Test that the schema contains required elements."""
@@ -15,6 +17,7 @@ def test_schema_contains_required_elements():
     assert "<actions>" in schema, "Schema should contain actions element"
     assert "<file_edits>" in schema, "Schema should contain file_edits element"
     assert "<shell_commands>" in schema, "Schema should contain shell_commands element"
+
 
 def test_get_schema_returns_string():
     """Test that get_schema returns a non-empty string."""
