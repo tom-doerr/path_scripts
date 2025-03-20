4ab749c18563d70451ea2969f37ca43619631e56
Tom Dörr <tomdoerr96@gmail.com>
2025-03-20 11:11:05 +0100

    style: Format XML string in test_pretty_format_xml for readability

--- tests/test_xml_tools.py
+++ tests/test_xml_tools.py
@@ -42,14 +42,16 @@ def test_extract_xml_multiple_matches():
 
 def test_pretty_format_xml():
     """Test XML formatting produces indented output."""
-    compressed_xml = "<response><message>Test</message><data><item>1</item></data></response>"
+    compressed_xml = (
+        "<response><message>Test</message><data><item>1</item></data></response>"
+    )
     formatted = pretty_format_xml(compressed_xml)
     # Check indentation
     assert formatted == (
-        '<response>\n'
-        '  <message>Test</message>\n'
-        '  <data>\n'
-        '    <item>1</item>\n'
-        '  </data>\n'
-        '</response>'
+        "<response>\n"
+        "  <message>Test</message>\n"
+        "  <data>\n"
+        "    <item>1</item>\n"
+        "  </data>\n"
+        "</response>"
     )
